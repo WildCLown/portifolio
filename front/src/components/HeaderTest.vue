c6270cadeecbd7d656f882f3c284b6fd465259f6
Gabriel <gtsa.wildclown@gmail.com>
2022-12-04 01:17:02 -0300

    header

---
+++ front/src/components/HeaderTest.vue
@@ -0,0 +1,22 @@
+<template>
+  <v-layout row wrap class="header">
+    <v-flex xs3>
+      <img :src="require('../assets/icons/GT.png')" style="max-width: 50px" />
+    </v-flex>
+    <v-flex style="color: white" class="ml-3"> Dale </v-flex>
+  </v-layout>
+</template>
+
+<script>
+export default {
+  name: "HeaderTest",
+  data() {
+    return {};
+  },
+};
+</script>
+<style scoped>
+.header {
+  background-color: #000d41;
+}
+</style>
