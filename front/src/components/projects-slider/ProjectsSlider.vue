cb8578397f0071b5f9803ddad8490d17ee09fa2b
Gabriel <gtsa.wildclown@gmail.com>
2024-03-10 01:03:37 -0300

    some changes

--- front/src/components/projects-slider/ProjectsSlider.vue
+++ front/src/components/projects-slider/ProjectsSlider.vue
@@ -18,30 +18,47 @@
           <v-sheet
             :color="project.textSide.backgroundColor"
             tile
+            style="height: 100%"
           >
             <v-layout raw wrap>
               <v-flex xs6>
-                <v-card outlined style="border: none; border-radius: 0;">
+                <v-card outlined style="border: none; border-radius: 0; height: 100%">
                   <v-card-title class="text-h6 justify-center" style="border-radius: 0;">
                     {{project.projectName}}
                   </v-card-title>
-                  <v-img
-                  :src="project.imageSrc"
-                  cover
-                  >
-                    
-                  </v-img>
+                  <v-card-text style="height: 100%" :style="project.imgSide.backgroundColor">
+                    <v-layout row wrap align-center style="height: 100%">
+                      <v-flex pa-6 mb-15>
+                        <v-img
+                          :src="project.imgSide.imageSrc"
+                        />
+                      </v-flex>
+                    </v-layout>
+                  </v-card-text>
                 </v-card>
               </v-flex>
               <v-flex 
                 xs6 
                 height="100%" 
-                pa-4
-                class="text-h5 text-center">
-                <span>
-                  {{ project.textSide.message }}
-                </span>
-                <p></p>
+                pa-4>
+                <v-layout
+                  row wrap
+                  class="text-h5"
+                  pa-5>
+                  <v-flex mb-10 xs12>
+                    {{ project.textSide.message }}
+                  </v-flex>
+                  <v-flex xs12 mb-10>
+                    <p>
+                      Local: {{ project.textSide.projectHosted}}
+                    </p>
+                  </v-flex>
+                  <v-flex v-if="project.textSide.buttonGoTo != null" class="text-center" xs12>
+                    <v-btn v-on:click="redirectPage(project.textSide.buttonGoTo)">
+                      Preview Project
+                    </v-btn>
+                  </v-flex>
+                </v-layout>
               </v-flex>
             </v-layout>
           </v-sheet>
@@ -51,13 +68,35 @@
   </template>
   
   <script>
-  import cyberImage from "@/assets/projectsImg/CyberSource.png"
+  import cyberImage from "@/assets/projectsImg/CyberSource.svg"
+  import stoolitLogo from "@/assets/projectsImg/Stoolit.png"
+
   export default {
     name: "ProjectsSlider",
     
     data() {
       return {
         projects: [
+          {
+            projectName: 'Stoolit',
+            textSide: {
+              backgroundColor: 'blue',
+              textColor: "white",
+              message: [
+                "Stoolit was a game developed which intends to provide user",
+                "an experience not only in main screen view, but also",
+                "in it's controller, providing mini-games and challenges when",
+                "interacting with objects through the gameplay.",
+                "An Unity project with Air console developed along with college friends."
+              ].join(" "),
+              projectHosted: "Private project",
+              buttonGoTo: "https://www.youtube.com/watch?v=ePslfyGNGEk&t"
+            },
+            imgSide: {
+              backgroundColor: 'background-color: grey;',
+              imageSrc: stoolitLogo
+            }
+          },
           {
             projectName: 'CyberSource 3DS',
             textSide: {
@@ -70,24 +109,41 @@
                 "In this project was able to prove leadership skills",
                 "and end to end architecture development using Kotlin."
               ].join(" "),
-              projectHosted: "Acqio",
-              buttonGoTo: "https://google.com.br"
+              projectHosted: "Acqio's Internal system",
+              buttonGoTo: null
             },
-            imageSrc: cyberImage
+            imgSide: {
+              backgroundColor: 'background-color: grey;',
+              imageSrc: cyberImage
+            }
           },
           {
-            projectName: 'Stoolit',
+            projectName: 'Bezier Curves',
             textSide: {
-              backgroundColor: 'blue',
+              backgroundColor: 'orange',
               textColor: "white",
-              message: "message text",
-              buttonGoTo: "https://google.com.br"
+              message: [
+                "Bezier Curves was a challenging project for college discipline",
+                "Graphic Processing, which purpose was create a tool that was able",
+                "to create bezier curves according to given ancor points, and giving",
+                "the number of interations that the curve may have."
+              ].join(" "),
+              projectHosted: "Here, go to Projects - Bezier Curves",
+              buttonGoTo: null
             },
-            imageSrc: "@/assets/projectsImg/CyberSource.png"
+            imgSide: {
+              backgroundColor: 'background-color: grey;',
+              imageSrc: stoolitLogo
+            }
           },
         ]
       }
     },
+    methods: {
+    redirectPage(link){
+      window.open(link, '_blank').focus()
+    }
+  },
   };
   </script>
   
