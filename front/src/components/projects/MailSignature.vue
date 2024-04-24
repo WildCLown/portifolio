e3f0c3745f0bcb2a304c43f1cb7716abd1e5572f
Gabriel <gtsa.wildclown@gmail.com>
2024-04-24 00:54:26 -0300

    working stuff

--- front/src/components/projects/MailSignature.vue
+++ front/src/components/projects/MailSignature.vue
@@ -158,6 +158,11 @@ export default {
         const file = new File([blob], 'wildclown.png', blob)
         let response = await this.fileToBase64(file)
         this.base64Src = `data:image/png;base64,${response}`
+
+        this.resizeImage(this.base64Src, 200, 200).then((result) => {
+            this.base64Src = result;
+        });
+
     })
 
     fetch(pepePhoto)
@@ -166,6 +171,10 @@ export default {
         const file = new File([blob], 'pepePhoto.png', blob)
         let response = await this.fileToBase64(file)
         this.base64Photo = `data:image/png;base64,${response}`
+
+        this.resizeImage(this.base64Photo, 400, 400).then((result) => {
+            this.base64Photo = result;
+        });
     })
   },
   computed: {
@@ -178,11 +187,6 @@ export default {
     },
   },
   methods: {
-    createImage(file) {
-        const reader = new FileReader();
-        reader.onloadend = () => (console.log(reader.result));
-        reader.readAsDataURL(file.target.files[0]);
-    },
     fileToBase64(file) {
         return new Promise((resolve, reject) => {
             const reader = new FileReader();
@@ -228,6 +232,36 @@ export default {
             }
         }
         setTimeout(() => { document.execCommand('copy'); }, 500);
+    },
+    resizeImage(base64Str, maxWidth = 400, maxHeight = 350) {
+        return new Promise((resolve) => {
+            let img = new Image()
+            img.src = base64Str
+            img.onload = () => {
+            let canvas = document.createElement('canvas')
+            const MAX_WIDTH = maxWidth
+            const MAX_HEIGHT = maxHeight
+            let width = img.width
+            let height = img.height
+
+            if (width > height) {
+                if (width > MAX_WIDTH) {
+                height *= MAX_WIDTH / width
+                width = MAX_WIDTH
+                }
+            } else {
+                if (height > MAX_HEIGHT) {
+                width *= MAX_HEIGHT / height
+                height = MAX_HEIGHT
+                }
+            }
+            canvas.width = width
+            canvas.height = height
+            let ctx = canvas.getContext('2d')
+            ctx.drawImage(img, 0, 0, width, height)
+            resolve(canvas.toDataURL())
+            }
+        })
     }
   }
 };
