b7a41423e4819416264e1b03f65ba67f25331f79
Gabriel Teixeira <gabriel.almeida@acqio.com.br>
2024-05-07 12:29:45 -0300

    Some changes

--- front/vue.config.js
+++ front/vue.config.js
@@ -1,19 +1,5 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  configureWebpack: ()=>{ if(process.env.NODE_ENV !== 'production') {
-      return {
-        entry: "./src/main.js",
-        devServer: {
-            hot: true,
-        },
-        watch: true,
-        watchOptions: {
-            ignored: /node_modules/,
-            poll: 1000,
-        },
-      }
-    }
-  },
   publicPath: '',
   transpileDependencies: [
     'vuetify'
