d01d31ceb19b33bbe00b6b734eba9620e1af172b
Gabriel Teixeira <32888444+WildCLown@users.noreply.github.com>
2024-05-14 21:54:35 -0300

    Update vue.config.js (#28)

--- front/vue.config.js
+++ front/vue.config.js
@@ -1,16 +1,5 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  configureWebpack: {
-    entry: "./src/main.js",
-    devServer: {
-        hot: true,
-    },
-    watch: true,
-    watchOptions: {
-        ignored: /node_modules/,
-        poll: 1000,
-    },
-  },
   publicPath: '',
   transpileDependencies: [
     'vuetify'
