399bdbe58491c6db8cad253f21865b4c04cb899b
Gabriel <gtsa.wildclown@gmail.com>
2024-05-29 00:40:58 -0300

    changes

--- front/src/components/personal-info/ProjectsSlider.vue
+++ front/src/components/personal-info/ProjectsSlider.vue
@@ -9,7 +9,6 @@
           :continuous="true"
           :show-arrows="true"
           hide-delimiters
-          :height="sliderSize"
         >
           <v-carousel-item
             v-for="(project, i) in projects"
@@ -19,6 +18,7 @@
             <v-sheet
               :color="project.textSide.backgroundColor"
               tile
+              style="height: 100%;"
             >
               <v-layout raw wrap fill-height>
                 <v-flex lg6 xs12 v-if="$vuetify.breakpoint.lgAndUp">
@@ -51,15 +51,11 @@
                 <v-flex 
                   lg6 xs12 
                 >
-                <v-layout
-                  row wrap
-                  :style="{color: project.textSide.textColor}"  
-                >
-                  <v-card
+                  <v-layout
+                    row wrap
                     :class="cardSize.textSize"
                     :style="{color: project.textSide.textColor}"
-                    class="font-weight-bold overflow-y-auto hiddenScroll"
-                    :max-height="sliderSize"
+                    class="font-weight-bold"
                   >
                     <v-flex 
                       xs12
@@ -68,26 +64,17 @@
                       v-if="!$vuetify.breakpoint.lgAndUp"
                     >
                       {{project.projectName}}
-                  </v-flex>
-                  
-                    <v-card-text
-                      :style="{background: project.textSide.backgroundColor}"
-                    >
-                      <div 
-                        class="px-15 text-subtitle-1"
-                        :style="{color: project.textSide.textColor}"
-                      >
-                        <p> {{ project.textSide.message }} </p>
-                        <p> Local: {{ project.textSide.projectHosted}} </p>
-                      </div>
-                      <div class="text-center">
-                        <v-btn v-on:click="redirectPage(project.textSide.buttonGoTo)">
-                          Preview Project
-                        </v-btn>
-                      </div>
-                    </v-card-text>
-                  </v-card>
-                </v-layout>
+                    </v-flex>
+                    <v-flex xs12 py-5 px-15>
+                      <p> {{ project.textSide.message }} </p>
+                      <p> Local: {{ project.textSide.projectHosted}} </p>
+                    </v-flex>
+                    <v-flex v-if="project.textSide.buttonGoTo != null" class="text-center" xs12>
+                      <v-btn v-on:click="redirectPage(project.textSide.buttonGoTo)">
+                        Preview Project
+                      </v-btn>
+                    </v-flex>
+                  </v-layout>
                 </v-flex>
               </v-layout>
             </v-sheet>
@@ -107,7 +94,6 @@
     
     data() {
       return {
-        sliderSize: 400,
         projects: [
           {
             projectName: 'Stoolit',
@@ -115,13 +101,11 @@
               backgroundColor: '#1E6F50',
               textColor: "white",
               message: [
-                "Stoolit was a college game developed which intends to provide user",
+                "Stoolit was a game developed which intends to provide user",
                 "an experience not only in main screen view, but also",
-                "in it's controller, showing mini-games and challenges when",
+                "in it's controller, providing mini-games and challenges when",
                 "interacting with objects through the gameplay.",
-                "Inspired by Time-attack games, and Payday, Stoolit is an Unity project",
-                "using Air console API, being it developed in C# and JS/HTML",
-                "along with friends in about 2 months."
+                "An Unity project with Air console developed along with college friends."
               ].join(" "),
               projectHosted: "Private project",
               buttonGoTo: "https://www.youtube.com/watch?v=ePslfyGNGEk&t"
@@ -221,14 +205,5 @@
   .hiddenScroll::-webkit-scrollbar {
     display: none;
   }
-  
-  .v-sheet.v-card {
-      border-radius: 0px;
-  }
-
-  .v-sheet.v-card .theme--dark.v-banner.v-sheet{
-    box-shadow: 0px 0px 0px 0px;
-  }
-
 </style>
   
