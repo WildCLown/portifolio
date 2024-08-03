e9051f281f361a29c1c16cead378b3fc76778e21
Gabriel Teixeira <gtsa.wildclown@gmail.com>
2024-08-03 18:22:54 -0300

    Changes

--- front/src/components.d.ts
+++ front/src/components.d.ts
@@ -7,10 +7,8 @@ export {}
 
 declare module 'vue' {
   export interface GlobalComponents {
-    AppFooter: typeof import('./components/AppFooter.vue')['default']
     Footer: typeof import('./components/Footer.vue')['default']
     Header: typeof import('./components/Header.vue')['default']
-    HelloWorld: typeof import('./components/HelloWorld.vue')['default']
     PersonalInfo: typeof import('./components/PersonalInfo.vue')['default']
     ProjectCarousel: typeof import('./components/ProjectCarousel.vue')['default']
     ProjectsRouter: typeof import('./components/ProjectsRouter.vue')['default']
